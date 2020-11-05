5ef9101c87967430ddf2200405b6311f998f4270
nastyavar99 <nastyavar99@gmail.com>
2020-11-05 22:25:38 +0300

    ex 2 corrected

--- HW5/2.py
+++ HW5/2.py
@@ -1,21 +1,25 @@
 class MinStack:
     def __init__(self):
         self.lst = []
-        self.min_elem = None
+        self.min_elem = []
 
     def push(self, x: int) -> None:
         self.lst.append(x)
-        if self.min_elem is None or self.min_elem > x:
-            self.min_elem = x
+        if not self.min_elem:
+            self.min_elem.append(x)
+        else:
+            if x < self.min_elem[-1]:
+                self.min_elem.append(x)
+            else:
+                self.min_elem.append(self.min_elem[-1])
 
     def pop(self) -> None:
         if self.lst:
             self.lst.pop()
-
-        self.min_elem = min(self.lst) if self.lst else None
+            self.min_elem.pop()
 
     def top(self) -> int:
         return self.lst[-1]
 
     def getMin(self) -> int:
-        return self.min_elem
+        return self.min_elem[-1]
