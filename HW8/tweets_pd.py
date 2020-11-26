487d2db6e5257a5ceef86a526369b819229dec87
nastyavar99 <nastyavar99@gmail.com>
2020-11-27 00:39:37 +0300

    file deleted

--- HW8/tweets_pd.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('tweets.csv')
-df = df.dropna()
-df = df[df['author_name'].apply(lambda n: len(str(n).split()) >= 2)]
-df = df.groupby('author_name').agg({'tweet_content': lambda t: min(t, key=len)})
-print(df)
-
